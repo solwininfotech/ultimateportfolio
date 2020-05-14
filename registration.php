319c92da6e48a04e50fc5d5981e6f54a2f13f3ad
Mayur Dhudasia <mmdhudasia@gmail.com>
2020-05-14 12:08:02 +0530

    initial relese

---
+++ registration.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Solwin Infotech
+ * Solwin UltimatePortfolio Extension
+ *
+ * @category   Solwin
+ * @package    Solwin_UltimatePortfolio
+ * @copyright  Copyright © 2006-2016 Solwin (https://www.solwininfotech.com)
+ * @license    https://www.solwininfotech.com/magento-extension-license/
+ */
+?>
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Solwin_UltimatePortfolio',
+    __DIR__
+);
